10d1c24d80f62c41164621873ef4098b4fa747c3
Jacqui Comstock <41028747+jacquicomstock@users.noreply.github.com>
2022-08-05 11:21:03 -0700

    Update 2_make.fasta_BSF.R

--- 0_dada2_denoise_taxonomy/2_make.fasta_BSF.R
+++ 0_dada2_denoise_taxonomy/2_make.fasta_BSF.R
@@ -11,15 +11,18 @@ taxa <- as.data.frame(read.csv("C:/Users/jacqu/Desktop/BIOSSCOPE_SizeFrac_dada2_
 SAR11_taxa <- taxa %>% filter(Order =="SAR11 clade")
 SAR202_taxa <- taxa %>% filter(Order =="SAR202 clade")
 cyano_taxa <- taxa %>% filter(Phylum == "Cyanobacteria")
+plastid_taxa <- taxa %>% filter(Order == "Chloroplast")
 
 #start by giving our seq headers more manageable names (ASV_1, ASV_2...)
 colnames <- c("ASV","Kingdom","Phylum","Class","Order","Family","Genus","Species")
 colnames(sar11_taxa) <- colnames
 colnames(sar202_taxa) <- colnames
 colnames(cyano_taxa) <- colnames
+colnames(plastid_taxa) <- colnames
 sar11_seqs <- sar11_taxa$ASV
 sar202_seqs <- sar202_taxa$ASV
 pro_seqs <- cyano_taxa$ASV
+plastid_seqs <- plastid_taxa$ASV
 
 sar11_headers <- vector(dim(sar11_taxa)[1], mode="character")
 for (i in 1:dim(sar11_taxa)[1]) {
@@ -36,11 +39,18 @@ for (i in 1:dim(cyano_taxa)[1]) {
   pro_headers[i] <- paste(">ASV", i, sep="_")
 }
 
+plastid_headers <- vector(dim(plastid_taxa)[1], mode="character")
+for (i in 1:dim(plastid_taxa)[1]) {
+  plastid_headers[i] <- paste(">ASV", i, sep="_")
+}
+
 # making and writing out a fasta of our final ASV seqs:
 sar11_fasta <- c(rbind(sar11_headers, sar11_seqs))
 sar202_fasta <- c(rbind(sar202_headers, sar202_seqs))
 pro_fasta <- c(rbind(pro_headers, pro_seqs))
+plastid_fasta <- c(rbind(plastid_headers, plastid_seqs))
 
 write(sar11_fasta, "C:/Users/jacqu/Desktop/dada2_output/BIOSFrac_SAR11.fasta")
 write(sar202_fasta, "C:/Users/jacqu/Desktop/dada2_output/BIOSFrac_SAR202.fasta")
 write(pro_fasta, "C:/Users/jacqu/Desktop/dada2_output/BIOSFrac_Cyano.fasta")
+write(plastid_fasta, "C:/Users/jacqu/Desktop/dada2_output/BIOSFrac_Plastid.fasta")
